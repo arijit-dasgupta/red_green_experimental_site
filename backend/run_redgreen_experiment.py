9b52f028743d299789f57de6f1cef4101f92760a
Arijit Dasgupta <arijit.dg@hotmail.com>
2026-04-07 18:32:15 -0400

    edited experiment data

--- backend/run_redgreen_experiment.py
+++ backend/run_redgreen_experiment.py
@@ -84,14 +84,14 @@ from apscheduler.triggers.interval import IntervalTrigger
 # EXPERIMENT CONFIGURATION - MODIFY THESE VARIABLES TO CUSTOMIZE EXPERIMENT
 #=============================================================================
 PATH_TO_DATA_FOLDER = 'trial_data'  #RELATIVE path to the folder containing all trial datasets
-DATASET_NAME = 'March102026_PointClick'  # Specific dataset folder name within PATH_TO_DATA_FOLDER
+DATASET_NAME = 'JTAP_Experiment_1'  # Specific dataset folder name within PATH_TO_DATA_FOLDER
 FAM_TRIAL_PREFIXES = ['F']  # Prefixes for familiarization trial folders
 # EXP_TRIAL_PREFIXES = ['CC_control', 'CC_surprise', 'UC_positive', 'UC_negative']  # Prefixes for experimental trial folders
 EXP_TRIAL_PREFIXES = ['T']  # Prefixes for experimental trial folders
-EXPERIMENT_RUN_VERSION = 'clickpilot_march12_2026_v0'  # Version identifier for this experiment run
+EXPERIMENT_RUN_VERSION = 'experiment_1_pilot_april72026'  # Version identifier for this experiment run
 COUNTERBALANCE_OUTCOMES = True # if True, then we randomly swap the red and green goals per trial, and save that data. If False, then we follow the red/green assignment as dictated in each JSON file
 # Timeout and Prolific URL can be overridden via Heroku Config Vars (e.g. for a new experiment run)
-_timeout_min = int(os.environ.get('TIMEOUT_PERIOD_MINUTES', '45'))
+_timeout_min = int(os.environ.get('TIMEOUT_PERIOD_MINUTES', '50'))
 TIMEOUT_PERIOD = timedelta(minutes=_timeout_min)  # Maximum time before session expires
 _check_min = int(os.environ.get('CHECK_TIMEOUT_INTERVAL_MINUTES', '5'))
 check_TIMEOUT_interval = timedelta(minutes=_check_min)  # How often to check for timeouts
